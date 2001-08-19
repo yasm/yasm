b619f63541013b484f3e85ccf7fb687f59cb00c1
Peter Johnson <peter@tortall.net>
2001-08-19 04:25:18 +0000

    Add check for <sys/queue.h> and specific checks for bogus implementations. Idea taken from OpenSSH-portable.
    svn path=/trunk/yasm/; revision=142

---
+++ acconfig.h
@@ -0,0 +1,16 @@
+/* $Id: acconfig.h,v 1.1 2001/08/19 04:25:18 peter Exp $ */
+
+#ifndef YASM_CONFIG_H
+#define YASM_CONFIG_H
+
+/* Generated automatically from acconfig.h by autoheader. */
+/* Please make your changes there */
+
+@TOP@
+
+/* Workaround for bad <sys/queue.h> implementations. */
+#undef HAVE_BOGUS_SYS_QUEUE_H
+
+@BOTTOM@
+
+#endif /* YASM_CONFIG_H */
