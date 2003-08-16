c013fc21d1e49f333a3ed669101462ae7e76c397
Peter Johnson <peter@tortall.net>
2003-08-16 05:37:23 +0000

    Fix for Perl 5.8.0 in UTF-8 locales.
    svn path=/trunk/yasm/; revision=1043

--- test_hd.pl
+++ test_hd.pl
@@ -1,5 +1,11 @@
-#! /usr/bin/env perl
+#!/usr/bin/env perl
+
 open(BFILE,$ARGV[0]) || die "Could not open \\$ARGV[0]\\: $!";
+
+# This is needed to run correctly on systems with Perl version 5.8.0 or higher
+# and a UTF-8 locale.  This so happens to describe Tyler's setup nicely. :)
+binmode(BFILE);
+
 while(!eof(BFILE)) {
     read(BFILE, $buffer, 1);
     printf("%02x \n", ord($buffer));
