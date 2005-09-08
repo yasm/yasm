412b006374e2572722e25831a8bb90968d1e6d7a
Peter Johnson <peter@tortall.net>
2005-09-08 02:00:08 +0000

    Fix VC build files.  - _stdint.h for VC build (both 64-bit and 32-bit)  - Handle spaces in genmodule/genmacro/re2c paths.
    Patches by: Brian Gladman <brg@gladman.plus.com>
    
    svn path=/trunk/yasm/; revision=1225

--- Mkfiles/vc/genmodule/run.bat
+++ Mkfiles/vc/genmodule/run.bat
@@ -1,6 +1,6 @@
 @echo off
 cd ..\..\..
 echo "" >temp.txt
-for /R modules %%n in (*.c) do find "_LTX_" %%n | find "_module" | find "= {" >>temp.txt
+for /R modules %%n in (*.c) do find "_LTX_" "%%n" | find "_module" | find "= {" >>temp.txt
 type temp.txt | %1 libyasm\module.in
 del temp.txt
