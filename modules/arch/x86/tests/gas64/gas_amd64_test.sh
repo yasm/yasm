267174e02b3c80a07bfe0133b9343ef3e8570a99
Peter Johnson <peter@tortall.net>
2005-09-25 20:19:05 +0000

    Move parsers/gas/tests/amd64 to arch/x86/tests/gas64, as the tests going here are really entirely implemented in the x86 arch.
    After this commit, gas_amd64_test.sh needs to be renamed.
    
    svn path=/trunk/yasm/; revision=1240

---
+++ modules/arch/x86/tests/gas64/gas_amd64_test.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# $Id$
+${srcdir}/out_test.sh x86_gas64_test modules/arch/x86/tests/gas64 "amd64 gas format" "-f elf -m amd64 -p gas" ".o"
+exit $?
--- modules/parsers/gas/tests/amd64/gas_amd64_test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# $Id$
-${srcdir}/out_test.sh gas_amd64_test modules/parsers/gas/tests/amd64 "gas-compat parser" "-f elf -m amd64 -p gas" ".o"
-exit $?
