5c79adc252a0327f7e6e1cf4ecca4e96625ebdd1
Peter Johnson <peter@tortall.net>
2005-09-28 06:07:24 +0000

    Update VC build files for GAS parser.
    svn path=/trunk/yasm/; revision=1253

--- Mkfiles/vc/re2c/run.bat
+++ Mkfiles/vc/re2c/run.bat
@@ -2,3 +2,4 @@ cd ..\..\..\
 %1 -s modules\arch\lc3b\lc3bid.re > lc3bid.c
 %1 -s modules\arch\x86\x86id.re > x86id.c
 %1 -s modules\parsers\nasm\nasm-token.re > nasm-token.c
+%1 -s modules\parsers\gas\gas-token.re > gas-token.c
