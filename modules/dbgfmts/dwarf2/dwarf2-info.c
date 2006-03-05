580e690532601ce392f8d0fccb5354f9d62ffe65
Peter Johnson <peter@tortall.net>
2006-03-05 09:19:25 +0000

    * dwarf2-info.c: Include config.h before looking for HAVE_UNISTD_H define.
    svn path=/trunk/yasm/; revision=1402

--- modules/dbgfmts/dwarf2/dwarf2-info.c
+++ modules/dbgfmts/dwarf2/dwarf2-info.c
@@ -24,6 +24,11 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#undef HAVE_CONFIG_H
+#endif
+
 /* Need either unistd.h or direct.h (on Windows) to prototype getcwd() */
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
