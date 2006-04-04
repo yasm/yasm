a5d91fba6a8f20465f6a8884207f98b241d993fe
Peter Johnson <peter@tortall.net>
2006-04-04 06:07:32 +0000

    Hook Python module into the build (even though it's pretty incomplete), enabled with --enable-python (defaults to auto-detect).
    Thanks to some magic in python-yasm/Makefile.inc and setup.py, this actually
    plays nicely with automake distcheck.
    
    * m4/pythonhead.m4: Script to find Python.h.
    * m4/Makefile.inc: Include it in distfiles.
    * configure.ac: Add --enable-python option and Python and Pyrex detection.
    * tools/Makefile.inc: Pull tools/python-yasm/Makefile.inc into build.
    * python-yasm/Makefile.inc: Add automake build magic for extension.
    * setup.py: Likewise.
    
    svn path=/trunk/yasm/; revision=1456

--- tools/python-yasm/setup.py
+++ tools/python-yasm/setup.py
@@ -3,27 +3,90 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
+from os.path import basename, join, exists
+import re
 
-setup(
-        name='yasm',
-        version='0.0',
-        description='Python bindings for Yasm',
-        author='Michael Urman',
-        url='http://www.tortall.net/projects/yasm',
-        ext_modules=[
-            Extension('yasm', ['yasm.pyx'],
-                depends=['bytecode.pxi',
-                         'coretype.pxi',
-                         'expr.pxi',
-                         'floatnum.pxi',
-                         'intnum.pxi',
-                         'symrec.pxi',
-                         'value.pxi'],
-                include_dirs=['../..'],
-                library_dirs=['../..'],
-                libraries=['yasm'],
-            ),
-        ],
-        cmdclass = dict(build_ext=build_ext),
-    )
-        
+def ReadSetup(filename):
+    """ReadSetup goes through filename and parses out the values stored
+    in the file.  Values need to be stored in a
+    \"key=value format\""""
+
+    #return val
+    opts = {}
+
+    fh = open(filename, 'r')
+    reobj = re.compile(r"([a-z]+)=(.+)")
+    for line in fh.readlines():
+        matchobj = reobj.search(line)
+
+        if matchobj is None:
+            raise "Error:  syntaxt error in setup.txt line: %s"%line
+
+        lab, val = matchobj.groups()
+        opts[lab] = val
+    return opts
+
+def ParseCPPFlags(str):
+    """parse the CPPFlags macro"""
+    incl_dir = []
+    cppflags = []
+
+    tokens = str.split()
+    # go through the list of compile flags.  treat search path args
+    # specially; otherwise just add to "extra_compile_args"
+    for tok in tokens:
+        if tok.startswith("-I"):
+            incl_dir.append(tok[2:])
+        else:
+            cppflags.append(tok)
+
+    return (incl_dir, cppflags)
+
+def ParseSources(str, srcdir):
+    """parse the Sources macro"""
+    sources = []
+
+    tokens = str.split()
+    # go through the list of source filenames
+    # do the dance of detecting if the source file is in the current
+    # directory, and if it's not, prepend srcdir
+    for tok in tokens:
+        fn = None
+        if tok.endswith(".c"):
+            fn = tok
+        elif tok.endswith(".y"):
+            fn = basename(tok)[:-2] + ".c"
+        else:
+            continue
+        if not exists(fn):
+            fn = join(srcdir, fn)
+        sources.append(fn)
+
+    return sources
+
+def RunSetup(incldir, cppflags, sources):
+    setup(
+          name='yasm',
+          version='0.0',
+          description='Python bindings for Yasm',
+          author='Michael Urman',
+          url='http://www.tortall.net/projects/yasm',
+          ext_modules=[
+                       Extension('yasm',
+                                 sources=sources,
+                                 extra_compile_args=cppflags,
+                                 include_dirs=incldir,
+                                 library_dirs=['.'],
+                                 libraries=['yasm'],
+                       ),
+                      ],
+          cmdclass = dict(build_ext=build_ext),
+         )
+
+if __name__ == "__main__":
+    opts = ReadSetup("python-setup.txt")
+    incldir, cppflags = ParseCPPFlags(opts["includes"])
+    sources = ParseSources(opts["sources"], opts["srcdir"])
+    sources.append('yasm_python.c')
+    RunSetup(incldir, cppflags, sources)
+
