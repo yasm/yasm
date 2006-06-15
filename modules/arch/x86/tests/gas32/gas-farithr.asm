447aa4a55c228c761d8e810e1abe5214cc03402e
Peter Johnson <peter@tortall.net>
2006-06-15 04:55:47 +0000

    Fix farith and farithp instructions for GAS mode (e.g. fsubr).
    The easiest way to fix these was to adjust the GAP input file to have NASM
    and GAS versions of these instructions.
    
    Tests added to check both NASM and GAS (farith.asm and gas-farithr.asm
    respectively).
    
    svn path=/trunk/yasm/; revision=1579

---
+++ modules/arch/x86/tests/gas32/gas-farithr.asm
@@ -0,0 +1,10 @@
+.text
+        fsub %st(0),%st(1)
+        fsubp %st(0),%st(1)
+        fsubr %st(0),%st(1)
+        fsubrp %st(0),%st(1)
+        fdiv %st(0),%st(1)
+        fdivp %st(0),%st(1)
+        fdivr %st(0),%st(1)
+        fdivrp %st(0),%st(1)
+
