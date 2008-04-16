01ef04cee1ef613feaadfacf33754e2778b9084a
Peter Johnson <peter@tortall.net>
2008-04-16 03:41:03 +0000

    Bump 0.7.x branch version to 0.7.1.
    svn path=/branches/yasm-0.7.x/; revision=2068

--- Mkfiles/dj/config.h
+++ Mkfiles/dj/config.h
@@ -119,7 +119,7 @@
 #define PACKAGE_BUILD "BRANCH"
 
 /* Define to internal version of this package. */
-#define PACKAGE_INTVER "0.7.0"
+#define PACKAGE_INTVER "0.7.1"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "yasm"
