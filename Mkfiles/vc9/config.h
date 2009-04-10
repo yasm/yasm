376a191ddc728ee68c3bbf7adda56dcd33374350
Peter Johnson <peter@tortall.net>
2009-04-10 05:34:14 +0000

    Bump version numbers for release.
    svn path=/tags/yasm-0.8.0/; revision=2195

--- Mkfiles/vc9/config.h
+++ Mkfiles/vc9/config.h
@@ -117,7 +117,7 @@
 #define PACKAGE_BUGREPORT "bug-yasm@tortall.net"
 
 /* Define to build version of this package. */
-#define PACKAGE_BUILD "BRANCH"
+#define PACKAGE_BUILD "2194"
 
 /* Define to internal version of this package. */
 #define PACKAGE_INTVER "0.8.0"
@@ -126,13 +126,13 @@
 #define PACKAGE_NAME "yasm"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yasm 0.8.x"
+#define PACKAGE_STRING "yasm 0.8.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yasm"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.8.x"
+#define PACKAGE_VERSION "0.8.0"
 
 /* Define if the C compiler supports function prototypes. */
 #define PROTOTYPES 1
@@ -156,7 +156,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.8.x"
+#define VERSION "0.8.0"
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
