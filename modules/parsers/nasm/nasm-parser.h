d965ef7d7a7df127821ae2a49e0a2a710db32a54
Peter Johnson <peter@tortall.net>
2010-01-19 06:56:08 +0000

    Split yasm_parser_nasm structure out to separate header.
    This is needed so that other parsers can get access to this structure without
    naming conflicts.
    
    Contributed by: Alexei Svitkine
    
    svn path=/trunk/yasm/; revision=2276

--- modules/parsers/nasm/nasm-parser.h
+++ modules/parsers/nasm/nasm-parser.h
@@ -27,6 +27,8 @@
 #ifndef YASM_NASM_PARSER_H
 #define YASM_NASM_PARSER_H
 
+#include "nasm-parser-struct.h"
+
 #define YYCTYPE         unsigned char
 
 #define MAX_SAVED_LINE_LEN  80
@@ -72,68 +74,17 @@ enum tokentype {
     NONE                /* special token for lookahead */
 };
 
-typedef union {
-    unsigned int int_info;
-    char *str_val;
-    yasm_intnum *intn;
-    yasm_floatnum *flt;
-    yasm_bytecode *bc;
-    uintptr_t arch_data;
-    struct {
-        char *contents;
-        size_t len;
-    } str;
-} yystype;
-#define YYSTYPE yystype
+enum nasm_parser_state {
+    INITIAL,
+    DIRECTIVE,
+    SECTION_DIRECTIVE,
+    DIRECTIVE2,
+    LINECHG,
+    LINECHG2,
+    INSTRUCTION
+};
 
-typedef struct yasm_parser_nasm {
-    int tasm;
-
-    /*@only@*/ yasm_object *object;
-
-    /* last "base" label for local (.) labels */
-    /*@null@*/ char *locallabel_base;
-    size_t locallabel_base_len;
-
-    /*@dependent@*/ yasm_preproc *preproc;
-    /*@dependent@*/ yasm_errwarns *errwarns;
-
-    /*@dependent@*/ yasm_linemap *linemap;
-
-    /*@null@*/ yasm_bytecode *prev_bc;
-
-    int save_input;
-
-    yasm_scanner s;
-    enum {
-        INITIAL,
-        DIRECTIVE,
-        SECTION_DIRECTIVE,
-        DIRECTIVE2,
-        LINECHG,
-        LINECHG2,
-        INSTRUCTION
-    } state;
-
-    int token;          /* enum tokentype or any character */
-    yystype tokval;
-    char tokch;         /* first character of token */
-
-    /* one token of lookahead; used sparingly */
-    int peek_token;     /* NONE if none */
-    yystype peek_tokval;
-    char peek_tokch;
-
-    /* Starting point of the absolute section.  NULL if not in an absolute
-     * section.
-     */
-    /*@null@*/ yasm_expr *absstart;
-
-    /* Current location inside an absolute section (including the start).
-     * NULL if not in an absolute section.
-     */
-    /*@null@*/ yasm_expr *abspos;
-} yasm_parser_nasm;
+#define YYSTYPE nasm_yystype
 
 /* shorter access names to commonly used parser_nasm fields */
 #define p_object        (parser_nasm->object)
