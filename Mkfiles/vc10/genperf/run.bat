40ab77b4d03c079010f66ce37355ec7e6ae253c1
Brian Gladman <brg@gladman.plus.com>
2010-02-24 11:26:40 +0000

    Make a further attempt to use the Windows registry to find Python
    svn path=/trunk/yasm/; revision=2294

--- Mkfiles/vc10/genperf/run.bat
+++ Mkfiles/vc10/genperf/run.bat
@@ -1,33 +1,18 @@
 cd ..\..\..
 @echo off
-for /f "usebackq tokens=2,3,4,5,6" %%f in (`reg query HKCR\Python.File\shell\open\command`) do (if %%f==REG_SZ echo %%g %%h %%i %%j >run_py.bat)
-goto ftry%errorlevel%
-
-:ftry0
-goto pyfound
-:ftry1
-for /f "usebackq tokens=2,3,4" %%f in (`reg query HKCR\Python.File\shell\open\command`) do (if %%f==REG_SZ echo %%g %%h >run_py.bat) 
-goto stry%errorlevel%
-
-:stry0
-goto pyfound
-:stry1
-goto pynotfound
-
-:pyfound
-if not exist run_py.bat goto pynotfound
-echo ... building with Python ...
-@echo on
-call run_py.bat modules\arch\x86\gen_x86_insn.py
 del run_py.bat
-@echo off
+for /f "usebackq tokens=2*" %%f in (`reg query HKCR\Python.File\shell\open\command`) do (if %%f==REG_SZ echo %%g >run_py.bat) 
+@echo on
+if exist run_py.bat goto pyfound
+echo ... building without Python ...
 goto end
 
-:pynotfound
-echo ... building without Python ...
+:pyfound
+echo ... building with Python ...
+call run_py.bat modules\arch\x86\gen_x86_insn.py
+del run_py.bat
 
 :end
-@echo on
 %1 x86insn_nasm.gperf x86insn_nasm.c
 %1 x86insn_gas.gperf x86insn_gas.c
 %1 modules\arch\x86\x86cpu.gperf x86cpu.c
