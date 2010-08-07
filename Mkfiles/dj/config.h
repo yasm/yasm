3b20772048b7b032271af6106f38feb750cc8f77
Peter Johnson <peter@tortall.net>
2010-08-07 16:37:52 +0000

    Bump version numbers for 1.1.0 release.
    svn path=/tags/yasm-1.1.0/; revision=2353

--- Mkfiles/dj/config.h
+++ Mkfiles/dj/config.h
@@ -119,7 +119,7 @@
 #define PACKAGE_BUGREPORT "bug-yasm@tortall.net"
 
 /* Define to build version of this package. */
-#define PACKAGE_BUILD "BRANCH"
+#define PACKAGE_BUILD "2352"
 
 /* Define to internal version of this package. */
 #define PACKAGE_INTVER "1.1.0"
@@ -128,13 +128,13 @@
 #define PACKAGE_NAME "yasm"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yasm 1.1.x"
+#define PACKAGE_STRING "yasm 1.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yasm"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.x"
+#define PACKAGE_VERSION "1.1.0"
 
 /* Define if the C compiler supports function prototypes. */
 #define PROTOTYPES 1
@@ -158,7 +158,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.x"
+#define VERSION "1.1.0"
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
