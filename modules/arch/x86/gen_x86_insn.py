5d5ad1d91c0707d03dbd7d24bbefb495cc76c02c
Peter Johnson <peter@tortall.net>
2011-06-27 23:48:28 -0700

    Allow 64-bit LFS/LGS/LSS.
    [#229 state:resolved]

--- modules/arch/x86/gen_x86_insn.py
+++ modules/arch/x86/gen_x86_insn.py
@@ -1729,7 +1729,7 @@ for sfx, sz in zip("wl", [16, 32]):
 add_insn("lds", "ldes", modifiers=[0xC5])
 add_insn("les", "ldes", modifiers=[0xC4])
 
-for sfx, sz in zip("wl", [16, 32]):
+for sfx, sz in zip("wlq", [16, 32, 64]):
     add_group("lfgss",
         suffix=sfx,
         cpu=["386"],
