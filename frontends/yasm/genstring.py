d3ea3ac89f48f0cc28c6c32507afeec6b594cbac
Peter Johnson <peter@tortall.net>
2014-08-10 12:32:50 -0700

    genstring.py: Make Python3-compatible.

--- frontends/yasm/genstring.py
+++ frontends/yasm/genstring.py
@@ -23,19 +23,23 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+import sys
+
+def lprint(s, f = sys.stdout, e = '\n') :
+    f.write(s + e)
 
 def file_to_string(fout, str_name, fin_name):
     from os.path import basename
-    print >>fout, "/* This file auto-generated from %s by genstring.py - don't edit it */\n" % basename(fin_name)
-    print >>fout, "static const char* %s[] = {" % str_name
-    print >>fout, "\n".join('    "%s",' %
-                            l.strip().replace('\\', '\\\\').replace('"', '\\"')
-                            for l in open(fin_name))
-    print >>fout, "};"
+    lprint("/* This file auto-generated from %s by genstring.py - don't edit it */\n" % basename(fin_name), f=fout)
+    lprint("static const char* %s[] = {" % str_name, f=fout)
+    lprint("\n".join('    "%s",' %
+                     l.strip().replace('\\', '\\\\').replace('"', '\\"')
+                     for l in open(fin_name)),
+           f=fout)
+    lprint("};", f=fout)
 
 if __name__ == "__main__":
-    import sys
     if len(sys.argv) != 4:
-        print >>sys.stderr, "Usage: genstring.py <string> <outfile> <file>"
+        lprint("Usage: genstring.py <string> <outfile> <file>", f=sys.stderr)
         sys.exit(2)
     file_to_string(open(sys.argv[2], "w"), sys.argv[1], sys.argv[3])
