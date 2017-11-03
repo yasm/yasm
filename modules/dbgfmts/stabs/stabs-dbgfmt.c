yasm/yasm

commit 4c33af41ec62c5884c85117f91dce8a6a893b154

Revamp error/warning handling, using a model similar to Python's internal exception handling.  There are now two layers an error or warning goes through before it hits the user: first an error is logged via yasm_error_set() (or yasm_warn_set() for a warning).  Only one error may be set, whereas multiple warnings can be set (yasm_warn_set maintains a linked list).  Then, calling yasm_errwarn_propagate() propagates any error and/or warning(s) to an errwarns structure and associates the errors/warnings with a line number at that time; this call also clears the pending errors/warnings and allows new ones to be set.  The propagate function can safely be called when there are no pending error/warnings.
In addition, there are some helper errwarn functions that allow clearing of
an error/warning without propagating, getting it separately, etc.

Still yet to be done: changing most/all uses of yasm_internal_error() into
yasm_error_set(YASM_ERROR_ASSERTION).

The main advantage this change has is making libyasm functions feel much
more library like, and separating the user code line numbers from the inner
function error handling (e.g. intnum create functions only needed the line
number to trigger errors; this is no longer required).

The set/propagate/etc functions use global data structures to avoid passing
around a pointer to every function.  This would need to be made thread-local
data in a threaded app.  Errwarns containers (that keep associated line
numbers) are no longer global, so multiple source streams can be processed
separately with no conflict (at least if there's only a single thread of
execution).

svn path=/trunk/yasm/; revision=1521

modules/dbgfmts/stabs/stabs-dbgfmt.c
@@ -104,6 +104,7 @@ typedef struct {
     yasm_bytecode *basebc;      /* base bytecode from which to track SLINEs */
 
     yasm_dbgfmt_stabs *dbgfmt_stabs;
+    yasm_errwarns *errwarns;
 } stabs_info;
 
 typedef struct {
@@ -299,7 +300,8 @@ stabs_dbgfmt_generate_sections(yasm_section *sect, /*@null@*/ void *d)
     /* handle first (pseudo) bc separately */
     stabs_dbgfmt_generate_n_fun(d, yasm_section_bcs_first(sect));
 
-    yasm_section_bcs_traverse(sect, d, stabs_dbgfmt_generate_bcs);
+    yasm_section_bcs_traverse(sect, info->errwarns, d,
+			      stabs_dbgfmt_generate_bcs);
 
     if (yasm__strcasecmp(sectname, ".text")==0) {
         /* Close out last function by appending a null SO stab after last bc */
@@ -314,7 +316,7 @@ stabs_dbgfmt_generate_sections(yasm_section *sect, /*@null@*/ void *d)
 }
 
 static void
-stabs_dbgfmt_generate(yasm_dbgfmt *dbgfmt)
+stabs_dbgfmt_generate(yasm_dbgfmt *dbgfmt, yasm_errwarns *errwarns)
 {
     yasm_dbgfmt_stabs *dbgfmt_stabs = (yasm_dbgfmt_stabs *)dbgfmt;
     stabs_info info;
@@ -333,30 +335,39 @@ stabs_dbgfmt_generate(yasm_dbgfmt *dbgfmt)
 	return;
 
     info.dbgfmt_stabs = dbgfmt_stabs;
+    info.errwarns = errwarns;
     info.lastline = 0;
     info.stabcount = 0;
     info.stab = yasm_object_get_general(dbgfmt_stabs->object, ".stab", 0, 4, 0,
 					0, &new, 0);
     if (!new) {
 	yasm_bytecode *last = yasm_section_bcs_last(info.stab);
-	if (last == NULL)
-	    yasm__error(yasm_section_bcs_first(info.stab)->line,
+	if (last == NULL) {
+	    yasm_error_set(YASM_ERROR_GENERAL,
 		N_("stabs debugging conflicts with user-defined section .stab"));
-	else
-	    yasm__warning(YASM_WARN_GENERAL, 0,
+	    yasm_errwarn_propagate(errwarns,
+				   yasm_section_bcs_first(info.stab)->line);
+	} else {
+	    yasm_warn_set(YASM_WARN_GENERAL,
 		N_("stabs debugging overrides empty section .stab"));
+	    yasm_errwarn_propagate(errwarns, 0);
+	}
     }
 
     info.stabstr = yasm_object_get_general(dbgfmt_stabs->object, ".stabstr", 0,
 					   1, 0, 0, &new, 0);
     if (!new) {
 	yasm_bytecode *last = yasm_section_bcs_last(info.stabstr);
-	if (last == NULL)
-	    yasm__error(yasm_section_bcs_first(info.stabstr)->line,
+	if (last == NULL) {
+	    yasm_error_set(YASM_ERROR_GENERAL,
 		N_("stabs debugging conflicts with user-defined section .stabstr"));
-	else
-	    yasm__warning(YASM_WARN_GENERAL, 0,
+	    yasm_errwarn_propagate(errwarns,
+				   yasm_section_bcs_first(info.stab)->line);
+	} else {
+	    yasm_warn_set(YASM_WARN_GENERAL,
 		N_("stabs debugging overrides empty section .stabstr"));
+	    yasm_errwarn_propagate(errwarns, 0);
+	}
     }
 
 

commit 8d8e591afda26c7e1188406ad6cc69347bba887e

fix info.stab / info.stabstr typo in stabs-dbgfmt.c
fixes issue #88. typo was introduced by commit 4c33af4 back in 2006.

modules/dbgfmts/stabs/stabs-dbgfmt.c
@@ -355,7 +355,7 @@ stabs_dbgfmt_generate(yasm_object *object, yasm_linemap *linemap,
             yasm_error_set(YASM_ERROR_GENERAL,
                 N_("stabs debugging conflicts with user-defined section .stabstr"));
             yasm_errwarn_propagate(errwarns,
-                                   yasm_section_bcs_first(info.stab)->line);
+                                   yasm_section_bcs_first(info.stabstr)->line);
         } else {
             yasm_warn_set(YASM_WARN_GENERAL,
                 N_("stabs debugging overrides empty section .stabstr"));